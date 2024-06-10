c6b7d142cc44a12277d50ec4bd91d92e2429c7bf
Racheal Ako <rachealako22@gmail.com>
2024-06-10 21:44:35 +0100

    i am done

--- index.js
+++ index.js
@@ -6,4 +6,5 @@ var restOfLetter = name.slice(1,name.lenght);
 var decapitalizedLetters = restOfLetter.toLowerCase();
 
 var capitalizeName = capitalizedFirstLetter + decapitalizedLetters;
+alert("Welcome to our space " + capitalizeName);
 console.log("Welcome to our space " + capitalizeName);
